475459afa6ed3d56784cf7208528e586c7e2cff1
sk0607gh <sunil0607.off@gmail.com>
2025-06-04 01:31:28 +0530

    Add files via upload

---
+++ May25/01-namedFunctions.js
@@ -0,0 +1,31 @@
+// Named Functions
+
+function helloWorld() {
+    
+        console.log("Hello World......");
+        
+}
+
+function addValue() {
+        
+        var     a = 10
+        let     b = 20;
+        return  c = a+b;
+                
+}
+
+
+helloWorld();
+
+/* For this u wont get anything displayed in the Console why bec it is returning a value which we need to get and 
+display it using Console.LOG */
+
+addValue();  
+
+//Getting the Return value, storing it in a VAR and printing the value that is stored in the Variable.
+var totValue = addValue();
+console.log( "SUM VALUE is : " + totValue );
+
+
+
+
---
+++ May25/02-AnonymousFunction.js
@@ -0,0 +1,9 @@
+// ANONYMOUS FUNCTION
+
+let sample = function() {
+
+    console.log("This is a example for the Anonymous Function....");
+    
+}
+
+sample();
