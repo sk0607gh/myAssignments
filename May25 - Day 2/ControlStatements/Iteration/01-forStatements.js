d848a4c06649cf41dd595a1e8fec99e44be09df7
sk0607gh <sunil0607.off@gmail.com>
2025-06-16 00:13:28 +0530

    Delete May25 - Day 2 directory

--- May25 - Day 2/ControlStatements/Iteration/01-forStatements.js
+++
@@ -1,9 +0,0 @@
-// May 25 Session  - Control Iteration -  FOR LOOP Statement
-
-for (let index = 0; index < 5; index++) {
-    
-         console.log("Index Value is...." + index);
-    
-}
-
-console.log("DONE...." );
--- May25 - Day 2/Operator/01-Ternary.js
+++
@@ -1,9 +0,0 @@
-let x=10;
-x+=5
-console.log("Value of + X is :"+x);  //O/P : 10+5 = 15
-
-x-=5
-console.log("Value of - X is :"+x);  //O/P : 15-5 = 10
-
-x*=5
-console.log("Value of * X is :"+x);  //O/P : 10*5 = 50
